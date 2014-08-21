4248b23577d4c6966d84262ad0fcc54b3f20051c
Anatoly A. Kazantsev <anatoly@zetaprints.com>
2014-08-21 17:19:20 +0800

    Return default values for metadata fields if they don't have values in attribute

--- app/code/community/MVentory/API/Model/Product/Attribute/Api.php
+++ app/code/community/MVentory/API/Model/Product/Attribute/Api.php
@@ -74,11 +74,7 @@ class MVentory_API_Model_Product_Attribute_Api
       'options' => $this->optionsPerStoreView($attr->getId(), $storeId)
     );
 
-    if ($attr['mventory_metadata']
-        && ($metadata = unserialize($attr['mventory_metadata'])) !== false)
-      $result += $metadata;
-
-    return $result;
+    return $result + $this->_prepareMetadata($attr);
   }
 
   public function fullInfoList ($setId) {
@@ -264,5 +260,42 @@ class MVentory_API_Model_Product_Attribute_Api
                  : $attr->getFrontendLabel();
 
     return $label != '~';
+  }
+
+  /**
+   * Deserialise and return metadata of it's available otherwise return
+   * empty array
+   *
+   * @param array $attr Attr data
+   * @return array Deserialised metadata
+   */
+  protected function _getMetadata ($attr) {
+    if (!$attr['mventory_metadata'])
+      return array();
+
+    return ($metadata = unserialize($attr['mventory_metadata'])) === false
+             ? array()
+               : $metadata;
+  }
+
+  /**
+   * Set default values for metadata fields if they don't have value in the
+   * attr's metadata
+   *
+   * @param array $attr Attr data
+   * @return array Prepared metadata
+   */
+  protected function _prepareMetadata ($attr) {
+    $metadata = $this->_getMetadata ($attr);
+    $defaults = (array) Mage::getConfig()->getNode(
+      'mventory/metadata',
+      'default'
+    );
+
+    foreach ($defaults as $field => $defValue)
+      if (!isset($metadata[$field]))
+        $metadata[$field] = (string) $defValue;
+
+    return $metadata;
   }
 }
