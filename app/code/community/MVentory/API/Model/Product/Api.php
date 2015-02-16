d0520f840b3d0251256105ce5807abbc7bf32056
Anatoly A. Kazantsev <anatoly@zetaprints.com>
2015-02-16 13:27:02 +0800

    Add missing category_ids field

--- app/code/community/MVentory/API/Model/Product/Api.php
+++ app/code/community/MVentory/API/Model/Product/Api.php
@@ -76,7 +76,8 @@ class MVentory_API_Model_Product_Api extends Mage_Catalog_Model_Product_Api {
       'product_id' => $productId,
       'sku' => $product->getSku(),
       'set' => $product->getAttributeSetId(),
-      'websites' => $product->getWebsiteIds()
+      'websites' => $product->getWebsiteIds(),
+      'category_ids' => $product->getCategoryIds()
     );
 
     $editableAttributes = $product
